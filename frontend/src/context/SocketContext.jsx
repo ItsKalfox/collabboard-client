1317a719cadef819f74cf1b8d6ec4825add936fe
DharmathilakaRCC <dhachanu24@gmail.com>
2026-09-12 17:47:54 +0530

    feat(realtime): create SocketContext boilerplate

---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useContext } from 'react';
+
+const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+  return (
+    <SocketContext.Provider value={{}}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
