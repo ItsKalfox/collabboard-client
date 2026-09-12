2251ecad4ff3a087921c9e577e52ed30a90d1528
DharmathilakaRCC <dhachanu24@gmail.com>
2026-09-12 17:54:19 +0530

    feat(realtime): emit leave_board event on component unmount

--- frontend/src/components/Board/KanbanBoard.jsx
+++ frontend/src/components/Board/KanbanBoard.jsx
@@ -69,6 +69,10 @@ export default function KanbanBoard({ projectId, refreshKey, currentProject, cur
   useEffect(() => {
     if (socket && projectId) {
       socket.emit('join_board', projectId);
+      
+      return () => {
+        socket.emit('leave_board', projectId);
+      };
     }
   }, [socket, projectId]);
 
